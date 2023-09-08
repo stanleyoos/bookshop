85a097527397285a521e36ca58479456db1177ec
stanleyos <stanislaw.sarka@gmail.com>
2023-09-08 12:34:41 +0200

    Configuration added - new packages and connection to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as cors from 'cors';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(cors()).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
