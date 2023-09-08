94ec9f39d6d822bbf7893d49a139c3a9dcd9d05b
stanleyos <stanislaw.sarka@gmail.com>
2023-09-08 12:35:49 +0200

    Added CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api');
   await app.enableShutdownHooks();
   await app.listen(3000);
 }
