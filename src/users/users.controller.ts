af5e1781155764d9e9766dc02ceaa43f0b486918
stanleyos <stanislaw.sarka@gmail.com>
2023-09-12 11:10:13 +0200

    Created User module with controller and service

---
+++ src/users/users.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get('/')
+  getAll() {
+    return this.usersService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const user = await this.usersService.getById(id);
+    if (!user) throw new NotFoundException('User not found!');
+    return user;
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,83 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/shared/services/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+
+  public async getAll(): Promise<User[]> {
+    return this.prismaService.user.findMany();
+  }
+
+  public async getById(id: User['id']): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: { id },
+    });
+  }
+
+  public async getByEmail(email: User['email']): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: { email },
+      include: { password: true },
+    });
+  }
+
+  public async createUser(
+    userData: Omit<User, 'id' | 'role'>,
+    password: string,
+  ) {
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...userData,
+          password: {
+            create: {
+              hashedPassword: password,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2002')
+        throw new ConflictException('Title is already taken');
+      throw error;
+    }
+  }
+
+  public async updateUser(
+    userId: User['id'],
+    userData: Omit<User, 'id' | 'role'>,
+    password: string | undefined,
+  ) {
+    try {
+      if (password) {
+        return await this.prismaService.user.update({
+          where: { id: userId },
+          data: {
+            ...userData,
+            password: {
+              update: {
+                hashedPassword: password,
+              },
+            },
+          },
+        });
+      }
+      return await this.prismaService.user.update({
+        where: { id: userId },
+        data: userData,
+      });
+    } catch (error) {
+      if (error.code === 'P2002')
+        throw new ConflictException('Title is already taken');
+      throw error;
+    }
+  }
+
+  public async deleteUser(id: User['id']) {
+    return this.prismaService.user.delete({
+      where: { id },
+    });
+  }
+}
